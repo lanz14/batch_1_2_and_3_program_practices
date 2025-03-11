ac1b382d452e42afc0eee4f67071824f37ff2969
Lance <enaganlance@gmail.com>
2025-03-11 13:18:09 +0800

    pseudocode

---
+++ batch_1_programming_practice/product_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+# Ask the user to input two numbers
+    # Must be integers only
+# The number 1 will be multiplied to number 2
+# Print now the product of the two numbers.
