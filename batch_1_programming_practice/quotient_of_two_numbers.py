4d2204804fd8dba03b71cc89f0f00b238728a24c
Lance <enaganlance@gmail.com>
2025-03-11 13:53:02 +0800

    pseudocode

---
+++ batch_1_programming_practice/quotient_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+# Ask the user to input two numbers
+    # Inputs are integers only
+# The first number divided by the second number
+# Get the quotient and then print the result
