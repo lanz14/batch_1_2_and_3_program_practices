cde0eca79e8929a01895b20d5b4e955dc1cfb7a1
Lance <enaganlance@gmail.com>
2025-03-11 21:46:18 +0800

    pseudocode

---
+++ batch_1_programming_practice/print_even_numbers.py
@@ -0,0 +1,4 @@
+# Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+# Using for loop with the range of (0,101)
+# If number is equal to the remainder of zero it will be printed as even number
