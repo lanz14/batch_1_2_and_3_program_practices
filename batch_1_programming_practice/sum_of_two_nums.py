812d8e140f1bd05d1fe9f3aa988650377a485d1b
Lance <enaganlance@gmail.com>
2025-03-11 13:06:22 +0800

    input two numbers

--- batch_1_programming_practice/sum_of_two_nums.py
+++ batch_1_programming_practice/sum_of_two_nums.py
@@ -4,3 +4,8 @@
     # The inputs are integers only.
 # The number 1 will be added to number 2
 # Print now the sum of the two numbers.
+
+print("\n(For inputs, integers are only acceptable)")
+
+number_1 = int(input("Input the first number: "))
+number_2 = int(input("Input the second number: "))
