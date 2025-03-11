41749a86559882a72fb957916054ca73cf80026a
Lance <enaganlance@gmail.com>
2025-03-11 21:19:04 +0800

    pseudocode

---
+++ batch_1_programming_practice/how_many_odd_numbers.py
@@ -0,0 +1,7 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# Ask the user to input ten numbers
+    # Adding a warning text to input integers only
+# If a number input is not equal to a remainder of zero it will be added as an odd number
+# Printing how many are the odd numbers
+# Indicating what odd numbers were entered
