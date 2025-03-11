6672dc22cff18f76213aa2e02ef3d45b65614ee4
Lance <enaganlance@gmail.com>
2025-03-12 00:39:08 +0800

    pseudocode

---
+++ batch_2_programming_practice/print_smaller_number.py
@@ -0,0 +1,8 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# User input of two numbers
+    # Displaying a text that the input must be integers only
+# elif statements:
+    # if number 1 is greater than number 2, print the smaller number is number 2.
+    # elif, number 1 is equal to number 2, print they are equal.
+    # else, print the smaller number is number 1.
