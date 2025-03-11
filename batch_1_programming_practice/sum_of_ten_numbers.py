ebc4cc55758a58dcb21a86544c5bfc6554f6eb2b
Lance <enaganlance@gmail.com>
2025-03-11 20:36:07 +0800

    pseudocode

---
+++ batch_1_programming_practice/sum_of_ten_numbers.py
@@ -0,0 +1,5 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# Implementing a for loop function in the range of 10
+    # Asking the user to input ten numbers
+# After obtaining the inputs get the sum
