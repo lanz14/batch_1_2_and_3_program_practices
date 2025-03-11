2edfec053be2da39b0032d93913085e89dfd254a
Lance <enaganlance@gmail.com>
2025-03-11 08:53:37 +0800

    pseudocode

---
+++ batch_1_programming_practice/print_equal_if_numbers_the_same.py
@@ -0,0 +1,8 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# Ask the user to input 2 numbers
+    # print a warning text that integers are the only acceptable inputs
+# Create an elif statements:
+    # If number 1 is greater than number 2 the output will be "Not Equal"
+    # elif number 1 is equal to number 2 the output will be "Equal"
+    # else, number 1 is less than number 2 the output will be "Not Equal"
