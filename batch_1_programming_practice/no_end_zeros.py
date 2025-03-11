1f30c19af3348b992509dc6c4ebde999b6f5f979
Lance <enaganlance@gmail.com>
2025-03-11 21:51:54 +0800

    pseudocode

---
+++ batch_1_programming_practice/no_end_zeros.py
@@ -0,0 +1,5 @@
+# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# Using for loop with the range of (0,101)
+# Number is divided by 10 
+    # If it is equal to the remainder of zero it will be not printed
