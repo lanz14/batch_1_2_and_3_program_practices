5d84ed1feeba633876085c86b7645cc9bfd9c8a9
Lance <enaganlance@gmail.com>
2025-03-11 13:55:23 +0800

    input two numbers

--- batch_1_programming_practice/quotient_of_two_numbers.py
+++ batch_1_programming_practice/quotient_of_two_numbers.py
@@ -4,3 +4,8 @@
     # Inputs are integers only
 # The first number divided by the second number
 # Get the quotient and then print the result
+
+print("(For inputs, integers are only acceptable)")
+
+number_1 = int(input("Input the first number: "))
+number_2 = int(input("Input the second number: "))
