39f3bfbb0acf59394bd7559a47f2637d346669fd
Lance <enaganlance@gmail.com>
2025-03-11 08:08:16 +0800

    pseudocode

---
+++ batch_1_programming_practice/print_bigger_number.py
@@ -0,0 +1,8 @@
+# Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# Ask the user to input two numbers
+    # Inputs are only integers
+# elif statements
+    # If the number 1 is greater than number 2, number 1 will be printed.
+    # If the number 1 is equal to number 2, the output will be they are both equal.
+    # else the number 2 will be printed as the bigger number, if these are false.
