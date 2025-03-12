0d0a5489e38d6a68cb2b61962194b9ea49b050d4
Lance <enaganlance@gmail.com>
2025-03-12 16:07:24 +0800

    warning text and user input

--- batch_2_programming_practice/difference_of_two_numbers.py
+++ batch_2_programming_practice/difference_of_two_numbers.py
@@ -1,6 +1,11 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # Adding a warning text that inputs must be in integers only
+print("\n(For inputs, integers are only acceptable)")
+
 # User input
+number_1 = int(input("\nInput the first number: "))
+number_2 = int(input("Input the second number: "))
+
 # number 1 minus number 2
 # Print the difference
