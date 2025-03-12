acd171b5f77534a0c3536055e507f9e7134619e4
Lance <enaganlance@gmail.com>
2025-03-12 16:21:55 +0800

    warning text and user input

---
+++ batch_2_programming_practice/quotient_without_decimal.py
@@ -0,0 +1,12 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+# Warning text
+print("\n(For inputs, integers are only acceptable)")
+
+# User input
+number_1 = int(input("\nInput the first number: "))
+number_2 = int(input("Input the second number: "))
+
+# Get the quotient
+
+# Print result without decimal point
