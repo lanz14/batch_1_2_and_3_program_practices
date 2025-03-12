a35a43c9c98c285d6042057c4fbe869923124f82
Lance <enaganlance@gmail.com>
2025-03-12 18:00:11 +0800

    get quotient and print result

--- batch_2_programming_practice/quotient_without_decimal.py
+++ batch_2_programming_practice/quotient_without_decimal.py
@@ -1,5 +1,7 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
+import math
+
 # Warning text
 print("\n(For inputs, integers are only acceptable)")
 
@@ -8,5 +10,11 @@ number_1 = int(input("\nInput the first number: "))
 number_2 = int(input("Input the second number: "))
 
 # Get the quotient
+quotient = number_1 / number_2
 
 # Print result without decimal point
+if quotient % 1 > 0:
+    rounded_up = math.ceil(quotient)
+    print(f"\nThe quotient is {quotient} rounded up to {rounded_up}")
+elif quotient % 1 == 0:
+        print(f"\nThe quotient is: {int(quotient)}")
