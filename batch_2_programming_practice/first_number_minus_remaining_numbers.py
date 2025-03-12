15aabb283f54013295b445c5076ec4f4aec6f47d
Lance <enaganlance@gmail.com>
2025-03-12 22:21:51 +0800

    user input

--- batch_2_programming_practice/first_number_minus_remaining_numbers.py
+++ batch_2_programming_practice/first_number_minus_remaining_numbers.py
@@ -1,6 +1,13 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # Create an empty list to store the numbers.
+numbers = []
+
 # User input of ten numbers
+print("Input ten numbers")
+for i in range(10):
+    inputs = float(input(f"Input number {i+1}: "))
+    numbers.append(inputs)
+
 # Calculating first number minus remaining nine numbers
 # Print result
