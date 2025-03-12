df289c6530d97a1a53ebf9350bbc6755fdc95984
Lance <enaganlance@gmail.com>
2025-03-12 23:22:12 +0800

    pseudocode

---
+++ batch_2_programming_practice/except_ending_zero_five.py
@@ -0,0 +1,5 @@
+# Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# Using while loop iterate numbers 0-100
+# Check if number is ending with 0 and 5
+# print result
