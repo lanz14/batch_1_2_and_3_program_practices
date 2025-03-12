2a7662752f08cac127ddc4b8b1af7c075e954f9d
Lance <enaganlance@gmail.com>
2025-03-12 23:28:24 +0800

    check if ends with zero and five, print result

--- batch_2_programming_practice/except_ending_zero_five.py
+++ batch_2_programming_practice/except_ending_zero_five.py
@@ -1,5 +1,10 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # iterate range of 1-100
+for numbers in range(0,101):
+
 # Check if number is ending with 0 and 5
+    if numbers % 5 != 0 and numbers % 10 != 0:
+
 # print result
+        print(numbers)
