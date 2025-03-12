74e59741f0b5e0f6a0a6a414300c7a4a8a5df6e8
Lance <enaganlance@gmail.com>
2025-03-12 16:04:24 +0800

    pseudocode

---
+++ batch_2_programming_practice/difference_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# Adding a warning text that inputs must be in integers only
+# User input
+# number 1 minus number 2
+# Print the difference
