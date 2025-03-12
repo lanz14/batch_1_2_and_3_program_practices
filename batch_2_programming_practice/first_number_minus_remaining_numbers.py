8e99f21a055b2c837fc8deb084798945a78737da
Lance <enaganlance@gmail.com>
2025-03-12 22:15:51 +0800

    pseudocode

---
+++ batch_2_programming_practice/first_number_minus_remaining_numbers.py
@@ -0,0 +1,6 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+# Create an empty list to store the numbers.
+# User input of ten numbers
+# Calculating first number minus remaining nine numbers
+# Print result
