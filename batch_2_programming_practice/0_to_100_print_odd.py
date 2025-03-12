07b5b2831d932d2109e21536560ae51de66587ba
Lance <enaganlance@gmail.com>
2025-03-12 23:11:23 +0800

    pseudocode

---
+++ batch_2_programming_practice/0_to_100_print_odd.py
@@ -0,0 +1,5 @@
+# Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+# Using while loop iterate numbers 0-100
+# Check if number is an odd
+# Print the result
