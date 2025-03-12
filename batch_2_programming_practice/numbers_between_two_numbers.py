0defadcbda76dfdb56a83b0a7cfb685947990a59
Lance <enaganlance@gmail.com>
2025-03-13 00:01:53 +0800

    pseudocode

---
+++ batch_2_programming_practice/numbers_between_two_numbers.py
@@ -0,0 +1,6 @@
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# Input two numbers
+# What is smaller and bigger number
+# When reaching the end of the last number, it will end
+# Printing numbers between the two numbers
