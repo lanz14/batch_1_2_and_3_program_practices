f71120f6d5a86f29fdaa357099741a8c3ef7464c
Lance <enaganlance@gmail.com>
2025-03-12 22:33:46 +0800

    pseudocode

---
+++ batch_2_programming_practice/how_many_even_numbers.py
@@ -0,0 +1,5 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# ask the user to input 10 numbers
+# if the entered number is equal to a remainder of 0 it will add as even number
+# print the result
