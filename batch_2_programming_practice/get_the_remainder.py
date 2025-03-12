e6e08abaecdb540268efafb1273bc9c4107bfae2
Lance <enaganlance@gmail.com>
2025-03-12 19:21:47 +0800

    pseudocode

---
+++ batch_2_programming_practice/get_the_remainder.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+# Add a warning text that integers are only acceptable inputs
+# User input
+# Get the remainder
+# Print result
