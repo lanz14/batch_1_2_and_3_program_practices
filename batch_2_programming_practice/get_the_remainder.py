00fe72f1fce4df2e5439cefd3648045988094cf3
Lance <enaganlance@gmail.com>
2025-03-12 19:23:13 +0800

    warning text and user input

--- batch_2_programming_practice/get_the_remainder.py
+++ batch_2_programming_practice/get_the_remainder.py
@@ -1,6 +1,12 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
 # Add a warning text that integers are only acceptable inputs
+print("\n(For inputs, integers are only acceptable)")
+
 # User input
+number_1 = int(input("\nInput the first number: "))
+number_2 = int(input("Input the second number: "))
+
 # Get the remainder
+
 # Print result
