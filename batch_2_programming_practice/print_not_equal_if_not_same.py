34a97fcbf471e7dc4d59de811cdbe77231464991
Lance <enaganlance@gmail.com>
2025-03-12 15:47:08 +0800

    added warning text, and user inputs

--- batch_2_programming_practice/print_not_equal_if_not_same.py
+++ batch_2_programming_practice/print_not_equal_if_not_same.py
@@ -1,7 +1,12 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 # Adding a warning text that inputs must be in integers only
+print("\n(For inputs, integers are only acceptable)")
+
 # User inputs
+number_1 = int(input("\nInput the first number: "))
+number_2 = int(input("Input the second number: "))
+
 # If number 1 is greater than number 2
 # Print "Not Equal"
 # If number 1 is less than number 2
