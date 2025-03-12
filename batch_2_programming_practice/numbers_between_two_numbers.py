fdc55183984761193c74117fd7ae361e14713763
Lance <enaganlance@gmail.com>
2025-03-13 00:03:16 +0800

    user input

--- batch_2_programming_practice/numbers_between_two_numbers.py
+++ batch_2_programming_practice/numbers_between_two_numbers.py
@@ -1,6 +1,9 @@
 # Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # Input two numbers
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+
 # What is smaller and bigger number
 # When reaching the end of the last number, it will end
 # Printing numbers between the two numbers
