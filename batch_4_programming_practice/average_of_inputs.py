5d97d22514c496e2b4c3f30d9ccd2c74d76d33fc
Lance <enaganlance@gmail.com>
2025-03-13 23:54:11 +0800

    user input

--- batch_4_programming_practice/average_of_inputs.py
+++ batch_4_programming_practice/average_of_inputs.py
@@ -1,7 +1,12 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 # empty list
+numbers = []
+
 # user input
+while True:
+    user_input = input("(Any non-numeric values will make the program terminate) Input a number: ")
+
 # sum all the inputs
     # divide it by the times of inputs
 # print the average
