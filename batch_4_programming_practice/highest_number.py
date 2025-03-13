372cec04f73fd63461681d2f43cec75ac59a6b58
Lance <enaganlance@gmail.com>
2025-03-13 23:05:21 +0800

    pseudocode

---
+++ batch_4_programming_practice/highest_number.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+# user input
+# check for the highest input
+# print the result
