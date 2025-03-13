6c3b306e753ba91a911c3dcd6c7844b25ef0becb
Lance <enaganlance@gmail.com>
2025-03-13 23:56:37 +0800

    getting the average, and print average

--- batch_4_programming_practice/average_of_inputs.py
+++ batch_4_programming_practice/average_of_inputs.py
@@ -7,6 +7,20 @@ numbers = []
 while True:
     user_input = input("(Any non-numeric values will make the program terminate) Input a number: ")
 
+
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        break
+
 # sum all the inputs
     # divide it by the times of inputs
+if numbers:
+    average = sum(numbers) / len(numbers)
+
 # print the average
+    print(f"The average is: {average}")
+
+else:
+    print("No valid numbers were entered.")
