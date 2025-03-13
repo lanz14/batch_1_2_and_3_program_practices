496991debf3df73a236ba4a333cf790fa3f89423
Lance <enaganlance@gmail.com>
2025-03-13 23:25:08 +0800

    user input

--- batch_4_programming_practice/highest_to_lowest.py
+++ batch_4_programming_practice/highest_to_lowest.py
@@ -1,6 +1,11 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
 # empty list
+numbers = []
+
 # user input
+while True:
+    user_input = input("(Any non-numeric values will make the program terminate) Input a number: ")
+
 # arrange from highest to lowest
 # display the sorted numbers
