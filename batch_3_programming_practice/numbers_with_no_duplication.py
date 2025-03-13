fedeeda4e5c1414c531c1a20521fb04d85f9d742
Lance <enaganlance@gmail.com>
2025-03-13 18:15:29 +0800

    pseudocode

---
+++ batch_3_programming_practice/numbers_with_no_duplication.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# Asking user to input ten numbers
+# Identify numbers that have no duplicates
+# Print result
