ed2cd740ec02fd40bcce3ee3491133b4712ef57b
Lance <enaganlance@gmail.com>
2025-03-13 21:44:57 +0800

    user input

--- batch_3_programming_practice/lowest_number.py
+++ batch_3_programming_practice/lowest_number.py
@@ -1,6 +1,11 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 # Empty list
+numbers = []
+
 # User input
+while True:
+    user_input = float(input("Input a number: "))
+
 # Check if inputs are valid
 # Display lowest number
