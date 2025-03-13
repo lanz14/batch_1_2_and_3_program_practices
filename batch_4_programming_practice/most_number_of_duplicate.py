9df3cc217b2a3a69fb44e0f8a876cebcafd7cd80
Lance <enaganlance@gmail.com>
2025-03-13 22:52:25 +0800

    pseudocode

---
+++ batch_4_programming_practice/most_number_of_duplicate.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+# empty set
+# ask the user to input a number
+# check what is the most entered number and print the result
