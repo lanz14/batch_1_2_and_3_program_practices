2874e00d796deac93ceda41f3db3a3ae62b0fe20
Lance <enaganlance@gmail.com>
2025-03-13 22:01:18 +0800

    if inputs are valid, and displat lowest number

--- batch_3_programming_practice/lowest_number.py
+++ batch_3_programming_practice/lowest_number.py
@@ -2,10 +2,22 @@
 
 # Empty list
 numbers = []
-
-# User input
+    
 while True:
-    user_input = float(input("Input a number: "))
+    user_input = (input("(Any non-numeric will quit the program) Input a number: "))
 
-# Check if inputs are valid
+# Check if inputs are valid  
+    try:
+        inputs = float(user_input)
+        numbers.append(user_input)
+            
+    except ValueError:
+        print("That's not a valid number. Program terminated.")
+        break
+
 # Display lowest number
+if numbers:
+    lowest_number = min(numbers)
+    print(f"\nThe lowest number you entered: {lowest_number}")
+else:
+    print("\nNo valid numbers were entered.")
