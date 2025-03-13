3dd78dfbba49aa1b2a1e938aa4b82d1fdb3b5e5e
Lance <enaganlance@gmail.com>
2025-03-13 21:03:53 +0800

    pseudocode

---
+++ batch_3_programming_practice/input_display_unique_duplicate.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+# Empty list
+# Asking the user to input numbers
+# Check if the input is a "Duplicate" or "Unique" upon inputting
+# Terminate the program if the input is invalid
