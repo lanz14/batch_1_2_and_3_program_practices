9cd0181b8145a9994d8cbab783e093bb44740578
Lance <enaganlance@gmail.com>
2025-03-13 18:21:15 +0800

    user input

--- batch_3_programming_practice/numbers_with_no_duplication.py
+++ batch_3_programming_practice/numbers_with_no_duplication.py
@@ -1,5 +1,10 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # Asking user to input ten numbers
+print("Input 10 numbers:")
+
+for inputs in range(10):
+    number = float(input(f"Enter number {inputs+1}: "))
+
 # Identify numbers that have no duplicates
 # Print result
