b587536e1b864b83d68e0c6fdbdf5249ff603368
Lance <enaganlance@gmail.com>
2025-03-13 23:52:26 +0800

    pseudocode

---
+++ batch_4_programming_practice/average_of_inputs.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+# empty list
+# user input
+# sum all the inputs
+    # divide it by the times of inputs
+# print the average
