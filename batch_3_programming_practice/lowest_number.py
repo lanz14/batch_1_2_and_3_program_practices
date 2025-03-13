aceda51e54c10a7fa19a388024b7afe4c03c86a9
Lance <enaganlance@gmail.com>
2025-03-13 21:42:47 +0800

    pseudocode

---
+++ batch_3_programming_practice/lowest_number.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+# Empty list
+# User input
+# Check if inputs are valid
+# Display lowest number
