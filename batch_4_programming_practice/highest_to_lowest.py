f3e65452288249d2e5cd6bd4f5f8b6f635393adf
Lance <enaganlance@gmail.com>
2025-03-13 23:38:17 +0800

    sorting from highest to lowest, and display result

--- batch_4_programming_practice/highest_to_lowest.py
+++ batch_4_programming_practice/highest_to_lowest.py
@@ -7,5 +7,21 @@ numbers = []
 while True:
     user_input = input("(Any non-numeric values will make the program terminate) Input a number: ")
 
+    try:
+        num = float(user_input)
+        numbers.append(num)
+
+    except ValueError:
+        print("Invalid input. Program terminated.")
+        break
+
 # arrange from highest to lowest
 # display the sorted numbers
+if numbers:
+    numbers.sort(reverse=True)
+    
+    print("\nSorted from highest to lowest:")
+    for num in numbers:
+        print(num)
+else:
+    print("No valid numbers were inputted.")
