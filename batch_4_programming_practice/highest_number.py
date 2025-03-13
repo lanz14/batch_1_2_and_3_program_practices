9d498ed07372683295c9309c6342fae0332d98b9
Lance <enaganlance@gmail.com>
2025-03-13 23:06:38 +0800

    user input

--- batch_4_programming_practice/highest_number.py
+++ batch_4_programming_practice/highest_number.py
@@ -1,5 +1,8 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 # user input
+while True:
+    num = int(input("Input a number: "))
+
 # check for the highest input
 # print the result
