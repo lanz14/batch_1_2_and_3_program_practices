2fde5e72cc33cd0e4f3e49b63a16bc32b94da557
Lance <enaganlance@gmail.com>
2025-03-13 23:12:40 +0800

    check for the highest number and display the result

--- batch_4_programming_practice/highest_number.py
+++ batch_4_programming_practice/highest_number.py
@@ -1,8 +1,21 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
+highest_number = None
+
 # user input
 while True:
-    num = int(input("Input a number: "))
-
+    try:
+        num = int(input("Input a number: "))
+        
 # check for the highest input
+        if highest_number is None or num > highest_number:
+            highest_number = num
+            
+    except ValueError:
+        break
+
 # print the result
+if highest_number is not None:
+    print(f"\nHighest number: {highest_number}")
+else:
+    print("No valid numbers were inputted")
