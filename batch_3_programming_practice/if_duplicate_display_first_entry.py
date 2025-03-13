e788dffafab573d87a645ed15559cc4fabbf2dd8
Lance <enaganlance@gmail.com>
2025-03-13 20:30:04 +0800

    pseudocode

---
+++ batch_3_programming_practice/if_duplicate_display_first_entry.py
@@ -0,0 +1,6 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# Initializing two empty lists
+# User input (10 numbers)
+# Print the user's input numbers
+# Print the numbers without duplication, keeping the first entry
