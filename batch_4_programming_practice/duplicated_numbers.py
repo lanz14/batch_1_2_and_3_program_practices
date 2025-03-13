bce70ca24bb88d9601fb6ef078ebd0ed876ca27e
Lance <enaganlance@gmail.com>
2025-03-13 22:28:27 +0800

    pseudocode

---
+++ batch_4_programming_practice/duplicated_numbers.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# empty sets
+# user input
+# check if there is a duplication
+# display duplicates
