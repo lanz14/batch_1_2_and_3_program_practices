ac95df2df3587469e60842f137ff45a48afeab2d
Lance <enaganlance@gmail.com>
2025-03-13 20:34:31 +0800

    user input

--- batch_3_programming_practice/if_duplicate_display_first_entry.py
+++ batch_3_programming_practice/if_duplicate_display_first_entry.py
@@ -1,6 +1,11 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
-# Initializing two empty lists
 # User input (10 numbers)
+print("Input 10 numbers:")
+for i in range(10):
+    num = int(input(f"Number {i+1}: "))
+
+# Initializing two empty lists
+
 # Print the user's input numbers
 # Print the numbers without duplication, keeping the first entry
