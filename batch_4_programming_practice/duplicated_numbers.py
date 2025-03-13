95fa9cbd420f21c02367b4bc7362e1f384d8a425
Lance <enaganlance@gmail.com>
2025-03-13 22:30:09 +0800

    user input

--- batch_4_programming_practice/duplicated_numbers.py
+++ batch_4_programming_practice/duplicated_numbers.py
@@ -1,6 +1,11 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # empty sets
+
 # user input
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+
 # check if there is a duplication
+
 # display duplicates
