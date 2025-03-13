8d24b1d4002bee9e30b35f9630f0a2a14cdf5fdb
Lance <enaganlance@gmail.com>
2025-03-13 22:33:47 +0800

    check for duplication, and display the result

--- batch_4_programming_practice/duplicated_numbers.py
+++ batch_4_programming_practice/duplicated_numbers.py
@@ -1,11 +1,20 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # empty sets
+numbers = []
+duplicates = set()
 
 # user input
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
 
 # check if there is a duplication
+    if num in numbers:
+        duplicates.add(num)
+    numbers.append(num) 
 
 # display duplicates
+if duplicates:
+    print("Duplicate numbers:", duplicates)
+else:
+    print("No duplicate numbers found.")
