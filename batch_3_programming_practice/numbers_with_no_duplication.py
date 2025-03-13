42079c38675906c55db3c16f8b576e14901be4c5
Lance <enaganlance@gmail.com>
2025-03-13 19:58:50 +0800

    identify duplication and print result

--- batch_3_programming_practice/numbers_with_no_duplication.py
+++ batch_3_programming_practice/numbers_with_no_duplication.py
@@ -1,10 +1,33 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # Asking user to input ten numbers
-print("Input 10 numbers:")
+def main():
+    numbers = []
 
-for inputs in range(10):
-    number = float(input(f"Enter number {inputs+1}: "))
+    print("Input 10 numbers:")
+
+    for inputs in range(10):
+        while True:
+            try:
+                number = float(input(f"Input number {inputs+1}: "))
+                numbers.append(number)
+                break
+            except ValueError:
+                print("Invalid input. Input a valid number")
 
 # Identify numbers that have no duplicates
+    no_duplicate_numbers = []
+    for num in numbers:
+        if numbers.count(num) == 1:
+            no_duplicate_numbers.append(num)
+
 # Print result
+    if no_duplicate_numbers:
+        print("\nNumbers that don't have duplicates:")
+        for num in no_duplicate_numbers:
+            print(num)
+    else:
+        print("\nThere are no numbers without duplicates.")
+
+if __name__ == "__main__":
+    main()
